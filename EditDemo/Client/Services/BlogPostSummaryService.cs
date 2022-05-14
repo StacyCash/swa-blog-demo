1a11e9fa86cf9ca864b2b6d716c7fccdd2dc64e6
Stacy Cashmore <33732891+StacyCash@users.noreply.github.com>
2022-05-14 15:08:07 +0200

    Add Edit Demo

---
+++ EditDemo/Client/Services/BlogPostSummaryService.cs
@@ -0,0 +1,27 @@
+﻿using Models;
+using Newtonsoft.Json;
+
+namespace Client.Services;
+
+public class BlogPostSummaryService
+{
+    private readonly HttpClient http;
+
+    public IEnumerable<BlogPost>? Summaries;
+
+    public BlogPostSummaryService(HttpClient http)
+    {
+        this.http = http ??
+            throw new ArgumentNullException(nameof(http));
+    }
+
+    public async Task LoadBlogPostSummaries()
+    {
+        if (Summaries == null)
+        {
+            var result = await http.GetStringAsync("api/blogposts ");
+            Summaries = JsonConvert.DeserializeObject<BlogPost[]>(result);
+        }
+    }
+
+}
