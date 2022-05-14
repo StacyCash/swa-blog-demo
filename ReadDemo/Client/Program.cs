601ae919bb84a9d33416ec399e9beed1d72a86a3
Stacy Cashmore <33732891+StacyCash@users.noreply.github.com>
2022-05-14 14:49:31 +0200

    Add display app

--- ReadDemo/Client/Program.cs
+++ ReadDemo/Client/Program.cs
@@ -1,4 +1,5 @@
 using Client;
+using Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<BlogPostSummaryService>();
+builder.Services.AddScoped<BlogPostService>();
 
 await builder.Build().RunAsync();
---
+++ ReadDemo/Client/Services/BlogPostService.cs
@@ -0,0 +1,43 @@
+﻿using Models;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+
+namespace Client.Services;
+
+public class BlogPostService
+{
+    private readonly HttpClient http;
+    private readonly NavigationManager navigationManager;
+    private List<BlogPost> blogPosts = new();
+
+    public BlogPostService(
+        HttpClient http,
+        NavigationManager navigationManager)
+    {
+        this.http = http ?? throw new ArgumentNullException(nameof(http));
+        this.navigationManager = navigationManager ?? throw new ArgumentNullException (nameof(navigationManager));
+    }
+
+    public async Task<BlogPost?> GetBlogPost(Guid blogPostId)
+    {
+        var result = await http.GetAsync($"api/blogposts/{blogPostId}");
+        if (!result.IsSuccessStatusCode)
+        {
+            navigationManager.NavigateTo("404");
+            return null;
+        }
+        var serializedPost = await result.Content.ReadAsStringAsync();
+        BlogPost? blogPost = JsonConvert.DeserializeObject<BlogPost>(serializedPost);
+
+        if (blogPost is null)
+        {
+            navigationManager.NavigateTo("404");
+            return null;
+        }
+
+        blogPosts.Add(blogPost);
+
+        return blogPosts.FirstOrDefault(bp => bp.Id == blogPostId);
+    }
+
+}
