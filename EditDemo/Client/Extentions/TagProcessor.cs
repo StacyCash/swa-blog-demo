86ebfec342ab849fb5596099d01043e7520efd63
Stacy Cashmore <33732891+StacyCash@users.noreply.github.com>
2022-05-24 15:52:23 +0200

    Trim tags for blog posts

---
+++ EditDemo/Client/Extentions/TagProcessor.cs
@@ -0,0 +1,8 @@
+﻿namespace Client.Extentions;
+
+public static class TagProcessor
+{
+    public static string[] CleanTags(this string[] tags){
+        return tags.ToList().Select(tag => tag.Trim()).Where(tag => !string.IsNullOrWhiteSpace(tag)).ToArray();
+    }
+}
